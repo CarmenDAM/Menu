0072876662d34e377b8609066712dcd6eb429b1b
CarmenDAM <cceballose01@educantabria.es>
2022-03-06 19:21:52 +0100

    1 file modified, 4 added

---
+++ src/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { NavbarWrapper } from "./styles/NavbarStyles";
+
+function Navbar({ open }) {
+  return (
+    <NavbarWrapper open={open}>
+      <a href="#">Link</a>
+      <a href="#">Link</a>
+      <a href="#">Link</a>
+      <a href="#">Link</a>
+    </NavbarWrapper>
+  );
+}
+
+import styled from "styled-components";
+
+export const NavbarWrapper = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background: turquoise;
+  position: fixed;
+  top: 10vh;
+  right: ${props => (props.open ? "0" : "-100%")};
+  width: 100%;
+  height: 90vh;
+  transition: right 0.3s linear;
+
+  @media only screen and (min-width: 624px) {
+    flex-direction: row;
+    position: initial;
+    height: auto;
+    justify-content: center;
+    background: white;
+  }
+
+  a {
+    padding: 0.5rem 0.8rem;
+    color: grey;
+    text-decoration: none;
+  }
+`;
+
+export default Navbar;
---
+++ src/styles/Header.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { HeaderWrapper } from "./styles/Header";
+import Navbar from "./Navbar";
+import MenuButton from "./MenuButton";
+
+function Header() {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <HeaderWrapper>
+      <h2>Logo</h2>
+      <Navbar open={open} />
+      <MenuButton open={open} handleClick={handleClick} />
+    </HeaderWrapper>
+  );
+}
+
+export default Header;
